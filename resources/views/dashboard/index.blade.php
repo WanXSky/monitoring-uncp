9529ee25983e7f60ab420e58cb50c0c2416e0b21
KenzoXploit <kenzorazor2112@gmail.com>
2026-01-01 04:41:45 +0800

    Kenjo

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -137,7 +137,7 @@
 <style>
   /* tinggi kira-kira 5 notifikasi */
   .dash-notif-scroll{
-    max-height: 840px;
+    max-height: 740px;
     overflow-y: auto;
     padding-right: 6px;
     overscroll-behavior: contain;
